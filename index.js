3e4ea469588e59baaadb713ac39d3253c38ed2e5
Vlamir Santo <vlamir.santo@dotlib.com.br>
2019-06-19 10:40:38 -0300

    Adicionando o Eslint e o EditorConfig no projeto.

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
 /**
  * Configurando o Nunjucks
  */
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
-app.set("view engine", "njk");
-app.use(express.urlencoded({ extended: false }));
+})
+app.set('view engine', 'njk')
+app.use(express.urlencoded({ extended: false }))
 
 /**
  * Middleware que checa se a idade foi digitada
  */
 const checkAgeExists = (req, res, next) => {
-  const { age } = req.query;
-  if (!age) return res.redirect("/");
+  const { age } = req.query
+  if (!age) return res.redirect('/')
 
-  return next();
-};
+  return next()
+}
 
 /**
  * Rota inicial
  */
-app.get("/", (req, res) => {
-  return res.render("index");
-});
+app.get('/', (req, res) => {
+  return res.render('index')
+})
 
 /**
  * Rota para checar a idade
  */
-app.get("/check", (req, res) => {
-  const { age } = req.query;
+app.get('/check', (req, res) => {
+  const { age } = req.query
 
-  if (age >= 18) return res.redirect(`/major?age=${age}`);
+  if (age >= 18) return res.redirect(`/major?age=${age}`)
 
-  return res.redirect(`/minor?age=${age}`);
-});
+  return res.redirect(`/minor?age=${age}`)
+})
 
 /**
  * Rota para menor de idade
  */
-app.get("/minor", checkAgeExists, (req, res) => {
-  const { age } = req.query;
+app.get('/minor', checkAgeExists, (req, res) => {
+  const { age } = req.query
 
-  return res.render("minor", { age });
-});
+  return res.render('minor', { age })
+})
 
 /**
  * Rota para maior de idade
  */
-app.get("/major", checkAgeExists, (req, res) => {
-  const { age } = req.query;
+app.get('/major', checkAgeExists, (req, res) => {
+  const { age } = req.query
 
-  return res.render("major", { age });
-});
+  return res.render('major', { age })
+})
 
-app.listen(3000);
+app.listen(3000)
